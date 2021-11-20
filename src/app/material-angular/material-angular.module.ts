ca1018272a46c3295f352ef2acd9ee291bf0118f
Ravi Chandra <jnsravichandra@gmail.com>
2021-11-20 15:05:59 +0530

    removed comments

--- src/app/material-angular/material-angular.module.ts
+++ src/app/material-angular/material-angular.module.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-//import { MatButtonModule } from '@angular/material/button';
 
 const MaterialComponents = [
   CommonModule
-  //MatButtonModule
 ];
 
 @NgModule({
