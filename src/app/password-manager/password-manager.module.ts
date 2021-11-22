8f7d8031f3df1d8b7b008f7100730ce6185de3ab
Ravi Chandra <jnsravichandra@gmail.com>
2021-11-22 21:47:51 +0530

    Added password manager module

---
+++ src/app/password-manager/password-manager.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialAngularModule } from '../material-angular/material-angular.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialAngularModule
+  ]
+})
+export class PasswordManagerModule { }
