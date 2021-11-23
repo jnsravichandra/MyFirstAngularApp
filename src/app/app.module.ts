c9411671721f3be44d44f4e7acc7a85baacfbc56
Ravi Chandra <jnsravichandra@gmail.com>
2021-11-23 13:09:20 +0530

    added firebase integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,17 @@ import { TourOfHerosModule } from './tour-of-heros/tour-of-heros.module';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialAngularModule } from './material-angular/material-angular.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -20,9 +30,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     MaterialAngularModule,
     TourOfHerosModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    
+    // Angular Firebase configuration
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()) 
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'my-first-angular-project-4de34',
+    appId: '1:138082667370:web:a98e6b22ff573e29df7313',
+    databaseURL: 'https://my-first-angular-project-4de34-default-rtdb.firebaseio.com',
+    storageBucket: 'my-first-angular-project-4de34.appspot.com',
+    apiKey: 'AIzaSyCXAV4s9bV-HldBh1oeM-NCUEOt0fZGn7s',
+    authDomain: 'my-first-angular-project-4de34.firebaseapp.com',
+    messagingSenderId: '138082667370',
+    measurementId: 'G-CT3DJJDWWN',
+  }
 };
 
 /*
